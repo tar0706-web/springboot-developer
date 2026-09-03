d765069f5f1308f97965b3c27d851c6be70c0d13
rdvwg5832-coder <rdvwg5832@gmail.com>
2026-09-03 15:43:17 +0900

    3주차

--- src/main/java/me/suk/springdeveloper/TestController.java
+++ src/main/java/me/suk/springdeveloper/TestController.java
@@ -1,18 +1,29 @@
 package me.suk.springdeveloper;
 
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class TestController {
     @GetMapping("/hi")
     public String hi () {
-        return "안녕하세요 'http://lcoalhost:8080/hi' 에 대한 응답입니다.";
+        return "안녕하세요 '안녕하세요? ' 에 대한 응답입니다.";
 
     }
     @GetMapping("/test")
-    public String test () {
-        return "안녕하세요 'http://lcoalhost:8080/hi' 에 대한 응답입니다.";
+    public String gtest () {
+        return "안녕하세요 '안녕하세요? /' 에 대한 응답입니다.";
+    }
+    @DeleteMapping("/test")
+    public String dtest () {
+        return "안녕하세요 '안녕하세요? /dlt' 에 대한 응답입니다.";
+    }
+    @PutMapping("/test")
+    public String puttest () {
+        return "안녕하세요 '안녕하세요? /Put' 에 대한 응답입니다.";
+    }
+    @PostMapping("/test")
+    public String posttest () {
+        return "안녕하세요 '안녕하세요? /Post' 에 대한 응답입니다.";
     }
 }
